d7a1bf4ee83208d21e4af93abc9f558224d94a86
Seán Bruton <sbruton@tcd.ie>
2016-04-20 21:41:31 +0100

    Added loading and sorting of pcd file paths

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 #include <pcl/console/parse.h>
 #include <boost/filesystem.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <map>
+#include <iomanip>
+
+
+namespace fs = boost::filesystem;
 
 constexpr int MAX_VALID_ARGS = 3;
 
-void printHelp (int argc, char **argv) {
+
+
+auto printHelp (int argc, char **argv) -> void {
   using pcl::console::print_error;
   using pcl::console::print_info;
 
@@ -16,30 +24,51 @@ void printHelp (int argc, char **argv) {
   print_info ("where x, y are the camera numbers (e.g. 12.yml).\n");
 }
 
-int main(int argc, char** argv) {
+auto getTimedPcdFilesInPath(fs::path pcd_dir) -> std::multimap<double, fs::path> {
+  auto result_set = std::multimap<double, fs::path>{};
+  for (auto const & entry : boost::make_iterator_range(fs::directory_iterator{pcd_dir})) {
+    if (fs::is_regular_file(entry.status())) {
+      if (entry.path().extension() == ".pcd") {
+        auto filename = entry.path().stem().string();
+        auto T_pos = filename.find_last_of("T");
+        if (T_pos != std::string::npos) {
+          auto time_str = filename.substr(T_pos + 1);
+          try {
+            auto time = boost::lexical_cast<double>(time_str);
+            result_set.emplace(time, entry);
+          } catch (boost::bad_lexical_cast &e) {
+            // Do nothing
+          }
+        }
+      }
+    }
+  }
+  return result_set;
+}
+
+auto main (int argc, char** argv) -> int {
   pcl::console::print_highlight (
       "PCL PCD cloud merging using saved extrinsics. "
                        "See %s -h for options.\n", argv[0]);
 
-  auto help_flag_1 = pcl::console::find_switch(argc, argv, "-h");
-  auto help_flag_2 = pcl::console::find_switch(argc, argv, "--help");
+  auto help_flag_1 = pcl::console::find_switch (argc, argv, "-h");
+  auto help_flag_2 = pcl::console::find_switch (argc, argv, "--help");
 
-  if(help_flag_1 || help_flag_2 || argc > MAX_VALID_ARGS) {
-    printHelp(argc, argv);
+  if (help_flag_1 || help_flag_2 || argc > MAX_VALID_ARGS) {
+    printHelp (argc, argv);
     return 0;
   }
 
   auto mls = pcl::console::find_switch(argc, argv, "-m");
 
   auto consumed_args = 1;
-  if(mls) {
+  if (mls) {
     pcl::console::print_info("Moving Least Squares smoothing active.\n");
     consumed_args++;
   }
 
-  namespace fs = boost::filesystem;
-  auto current_dir = fs::path{};
-  if(consumed_args == argc)
+  auto current_dir = fs::path {};
+  if (consumed_args == argc)
     current_dir = fs::system_complete(argv[0]).parent_path();
   else {
     auto path_str = std::string{argv[consumed_args]};
@@ -54,7 +83,7 @@ int main(int argc, char** argv) {
     }
   }
 
-  if(!fs::exists(current_dir) || !fs::is_directory(current_dir) ) {
+  if (!fs::exists(current_dir) || !fs::is_directory(current_dir) ) {
     pcl::console::print_error("A valid directory was not specified.\n");
     printHelp(argc, argv);
     return -1;
@@ -62,24 +91,21 @@ int main(int argc, char** argv) {
 
   // Find the relevant yml files and test for the corresponding directories
   auto yml_files = std::vector<fs::path>{};
-  auto it = fs::directory_iterator{current_dir};
-  while (it != fs::directory_iterator{}) {
-    if(fs::is_regular_file(*it)) {
-      auto file_name = it->path().filename().string();
-      auto file_type = it->path().extension();
-      // TODO: Hard coded - assumes num cameras < 10
-      auto file_suffix = file_name.substr(0, 2);
-      if(file_type == ".yml") {
+  for (auto const & entry : boost::make_iterator_range(fs::directory_iterator{current_dir})) {
+    if (fs::is_regular_file(entry)) {
+      auto file_type = entry.path().extension();
+      if (file_type == ".yml") {
+        auto file_name = entry.path().filename().string();
+        // TODO: Hard coded - assumes num cameras < 10
+        auto file_suffix = file_name.substr(0, 2);
         try {
           auto number = boost::lexical_cast<int>(file_suffix);
-          std::cout << it->path() << std::endl;
-          yml_files.push_back(it->path());
+          yml_files.push_back(entry.path());
         } catch (boost::bad_lexical_cast &e) {
           // Do nothing
         }
       }
     }
-    it++;
   }
 
   std::sort(yml_files.begin(), yml_files.end());
@@ -89,8 +115,8 @@ int main(int argc, char** argv) {
   auto same_target = true;
   auto target_num = -1;
 
-  for(const auto& d : yml_files) {
-    if(target_num == -1) {
+  for (const auto& d : yml_files) {
+    if (target_num == -1) {
       target_num = std::stoi(d.filename().string().substr(1, 2));
     }
     else if (target_num != std::stoi(d.filename().string().substr(1, 2))) {
@@ -108,53 +134,37 @@ int main(int argc, char** argv) {
 
   auto valid_all_source_dirs = true;
 
-  for(const auto& d : source_dirs) {
+  for (const auto& d : source_dirs) {
     auto valid_source_dir = fs::exists(d) && fs::is_directory(d);
     valid_all_source_dirs = valid_all_source_dirs && valid_source_dir;
   }
 
-  if(!valid_target_dir || !valid_all_source_dirs) {
+  if (!valid_target_dir || !valid_all_source_dirs) {
     pcl::console::print_error("No valid directories located.\n"
                                   "Directories should be named 1/, 2/ etc.\n");
     printHelp(argc, argv);
     return -1;
   }
 
-//  auto sub_dirs = std::vector<fs::path>{};
-//  auto yml_files = std::vector<fs::path>{};
-//  auto it = fs::directory_iterator{current_dir};
-//  while (it != fs::directory_iterator{}) {
-//    if(fs::is_directory(*it)) {
-//      try {
-//        auto dir_name = it->path().filename().string();
-//        auto number = boost::lexical_cast<int>(dir_name);
-//        sub_dirs.push_back(it->path());
-//      } catch (boost::bad_lexical_cast &e) {
-////        std::cout << "Not a valid directory\n" << std::endl;
-//      }
-//    }
-//    else if(fs::is_regular_file(*it)) {
-//      auto file_name = it->path().filename().string();
-//      auto file_type = file_name.substr((file_name.find_last_of(".") + 1));
-//      // TODO: Hard coded - assumes num cameras < 10
-//      auto file_suffix = file_name.substr(2);
-//      if(file_type == "yml") {
-//        try {
-//          auto number = boost::lexical_cast<int>(file_suffix);
-//        } catch {
-//
-//        }
-//      }
-//
-//
-//    }
-//    it++;
-//  }
+  auto ss = std::stringstream{};
+  ss << "merged";
+  if (mls)
+    ss << "-mls";
 
+  try {
+    fs::create_directory(current_dir / ss.str());
+  } catch(fs::filesystem_error e){
+    pcl::console::print_error("Unable to create directories. "
+                                  "Please ensure that the correct permissions are "
+                                  "set for the target folder\n");
+    printHelp(argc, argv);
+    return -1;
+  }
 
+  auto target_set = getTimedPcdFilesInPath(target_dir);
 
-
-  pcl::console::print_info("Current dir: %s\n", current_dir.string().c_str());
+  for (auto const & p : target_set)
+    std::cout << p.first << " --- " << p.second << std::endl;
 
   return 0;
 }
